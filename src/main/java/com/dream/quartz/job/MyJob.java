1fe9427a2b83f18a81796d5ffa09f7ea4735ab1a
wubo <343618924@qq.com>
2019-01-25 11:45:43 +0800

    quartz 参数获取设置

--- src/main/java/com/dream/quartz/job/MyJob.java
+++ src/main/java/com/dream/quartz/job/MyJob.java
@@ -1,6 +1,8 @@
 package com.dream.quartz.job;
 
+import com.alibaba.fastjson.JSON;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
@@ -14,6 +16,8 @@ import org.quartz.JobExecutionException;
 public class MyJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("你好啊");
+        JobDataMap jobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+        Object params = jobDataMap.get("params");
+        System.out.println("你好啊 : 参数为 --" + JSON.toJSONString(params));
     }
 }
--- src/main/java/com/dream/quartz/utils/QuartzUtils.java
+++ src/main/java/com/dream/quartz/utils/QuartzUtils.java
@@ -14,230 +14,187 @@ import org.quartz.TriggerKey;
 
 /**
  * Quartz调度管理器
- * 
  */
 public class QuartzUtils {
-	private static String JOB_GROUP_NAME = "JOB_GROUP_SYSTEM";
-	private static String TRIGGER_GROUP_NAME = "TRIGGER_GROUP_SYSTEM";
+    private static String JOB_GROUP_NAME = "JOB_GROUP_SYSTEM";
+    private static String TRIGGER_GROUP_NAME = "TRIGGER_GROUP_SYSTEM";
 
-	/**
-	 * @Description: 添加一个定时任务，使用默认的任务组名，触发器名，触发器组名
-	 * 
-	 * @param sched
-	 *            调度器
-	 * 
-	 * @param jobName
-	 *            任务名
-	 * @param cls
-	 *            任务
-	 * @param params
-	 *            任务参数
-	 * @param time
-	 *            时间设置，参考quartz说明文档
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void addJob(Scheduler sched, String jobName, @SuppressWarnings("rawtypes") Class cls, Object params,
-			String time) {
-		try {
-			JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);// 任务名，任务组，任务执行类
-			@SuppressWarnings("unchecked")
+    /**
+     * @param scheduler 调度器
+     * @param jobName   任务名
+     * @param cls       任务
+     * @param params    任务参数
+     * @param time      时间设置，参考quartz说明文档
+     * @Description: 添加一个定时任务，使用默认的任务组名，触发器名，触发器组名
+     * @Title: QuartzManager.java
+     */
+    public static void addJob(Scheduler scheduler, String jobName, @SuppressWarnings("rawtypes") Class cls, Object params, String time) {
+        try {
+            JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);// 任务名，任务组，任务执行类
+            JobDataMap jobDataMap = new JobDataMap();
+            jobDataMap.put("params", params);
+            JobDetail jobDetail = newJob(cls).withIdentity(jobKey).setJobData(jobDataMap).build();
+            TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);// 触发器
+            Trigger trigger = newTrigger().withIdentity(triggerKey).withSchedule(cronSchedule(time)).build();// 触发器时间设定
+            scheduler.scheduleJob(jobDetail, trigger);
+            if (!scheduler.isShutdown()) {
+                scheduler.start();// 启动
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-			JobDataMap jobDataMap = new JobDataMap();
-			jobDataMap.put("params", params);
-			JobDetail jobDetail = newJob(cls).withIdentity(jobKey).setJobData(jobDataMap).build();
-			TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);// 触发器
-			
-			Trigger trigger = newTrigger().withIdentity(triggerKey).withSchedule(cronSchedule(time)).build();// 触发器时间设定
-			sched.scheduleJob(jobDetail, trigger);
-			if (!sched.isShutdown()) {
-				sched.start();// 启动
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler        调度器
+     * @param jobName          任务名
+     * @param jobGroupName     任务组名
+     * @param triggerName      触发器名
+     * @param triggerGroupName 触发器组名
+     * @param jobClass         任务
+     * @param params           任务参数
+     * @param time             时间设置，参考quartz说明文档
+     * @Description: 添加一个定时任务
+     * @Title: QuartzManager.java
+     */
+    public static void addJob(Scheduler scheduler, String jobName, String jobGroupName, String triggerName,
+                              String triggerGroupName, @SuppressWarnings("rawtypes") Class jobClass, Object params, String time) {
+        try {
+            JobKey jobKey = new JobKey(jobName, jobGroupName);
+            JobDataMap jobDataMap = new JobDataMap();
+            jobDataMap.put("params", params);
+            @SuppressWarnings("unchecked")
+            JobDetail jobDetail = newJob(jobClass).withIdentity(jobKey).setJobData(jobDataMap).build();
+            // 触发器
+            TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
+            Trigger trigger = newTrigger().withIdentity(triggerKey).withSchedule(cronSchedule(time)).build();
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description: 添加一个定时任务
-	 * 
-	 * @param sched
-	 *            调度器
-	 * 
-	 * @param jobName
-	 *            任务名
-	 * @param jobGroupName
-	 *            任务组名
-	 * @param triggerName
-	 *            触发器名
-	 * @param triggerGroupName
-	 *            触发器组名
-	 * @param jobClass
-	 *            任务
-	 * @param params
-	 *            任务参数
-	 * @param time
-	 *            时间设置，参考quartz说明文档
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void addJob(Scheduler sched, String jobName, String jobGroupName, String triggerName,
-			String triggerGroupName, @SuppressWarnings("rawtypes") Class jobClass,  Object params, String time) {
-		try {
-			JobKey jobKey = new JobKey(jobName, jobGroupName);
-			JobDataMap jobDataMap = new JobDataMap();
-			jobDataMap.put("params", params);
-			@SuppressWarnings("unchecked")
-			JobDetail jobDetail = newJob(jobClass).withIdentity(jobKey).setJobData(jobDataMap).build();
-			// 触发器
-			TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
-			Trigger trigger = newTrigger().withIdentity(triggerKey).withSchedule(cronSchedule(time)).build();
-			sched.scheduleJob(jobDetail, trigger);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler 调度器
+     * @param jobName
+     * @param time
+     * @Description: 修改一个任务的触发时间(使用默认的任务组名 ， 触发器名 ， 触发器组名)
+     * @Title: QuartzManager.java
+     */
+    public static void modifyJobTime(Scheduler scheduler, String jobName, String time) {
+        try {
+            TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);
+            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+            if (trigger == null) {
+                return;
+            }
+            String oldTime = trigger.getCronExpression();
+            if (!oldTime.equalsIgnoreCase(time)) {
+                JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);
+                JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+                Class objJobClass = jobDetail.getJobClass();
+                Object params = jobDetail.getJobDataMap().get("params");
+                removeJob(scheduler, jobName);
+                System.out.println("修改任务：" + jobName);
+                addJob(scheduler, jobName, objJobClass, params, time);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description: 修改一个任务的触发时间(使用默认的任务组名，触发器名，触发器组名)
-	 * 
-	 * @param sched
-	 *            调度器
-	 * @param jobName
-	 * @param time
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	@SuppressWarnings("rawtypes")
-	public static void modifyJobTime(Scheduler sched, String jobName, String time) {
-		try {
-			TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);
-			CronTrigger trigger = (CronTrigger) sched.getTrigger(triggerKey);
-			if (trigger == null) {
-				return;
-			}
-			String oldTime = trigger.getCronExpression();
-			if (!oldTime.equalsIgnoreCase(time)) {
-				JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);
-				JobDetail jobDetail = sched.getJobDetail(jobKey);
-				Class objJobClass = jobDetail.getJobClass();
-				Object params = jobDetail.getJobDataMap().get("params");
-				removeJob(sched, jobName);
-				System.out.println("修改任务：" + jobName);
-				addJob(sched, jobName, objJobClass, params,time);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler        调度器 *
+     * @param scheduler        调度器
+     * @param triggerName
+     * @param triggerGroupName
+     * @param time
+     * @Description: 修改一个任务的触发时间
+     * @Title: QuartzManager.java
+     */
+    public static void modifyJobTime(Scheduler scheduler, String triggerName, String triggerGroupName, String time) {
+        try {
+            TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
+            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+            if (trigger == null) {
+                return;
+            }
+            String oldTime = trigger.getCronExpression();
+            if (!oldTime.equalsIgnoreCase(time)) {
+                // 修改时间
+                trigger.getTriggerBuilder().withSchedule(cronSchedule(time));
+                // 重启触发器
+                scheduler.resumeTrigger(triggerKey);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description: 修改一个任务的触发时间
-	 * 
-	 * @param sched
-	 *            调度器 *
-	 * @param sched
-	 *            调度器
-	 * @param triggerName
-	 * @param triggerGroupName
-	 * @param time
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void modifyJobTime(Scheduler sched, String triggerName, String triggerGroupName, String time) {
-		try {
-			TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
-			CronTrigger trigger = (CronTrigger) sched.getTrigger(triggerKey);
-			if (trigger == null) {
-				return;
-			}
-			String oldTime = trigger.getCronExpression();
-			if (!oldTime.equalsIgnoreCase(time)) {
-				// 修改时间
-				trigger.getTriggerBuilder().withSchedule(cronSchedule(time));
-				// 重启触发器
-				sched.resumeTrigger(triggerKey);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler 调度器
+     * @param jobName
+     * @Description: 移除一个任务(使用默认的任务组名 ， 触发器名 ， 触发器组名)
+     * @Title: QuartzManager.java
+     */
+    public static void removeJob(Scheduler scheduler, String jobName) {
+        try {
+            TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);
+            scheduler.pauseTrigger(triggerKey);// 停止触发器
+            scheduler.unscheduleJob(triggerKey);// 移除触发器
+            JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);
+            scheduler.deleteJob(jobKey);// 删除任务
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description: 移除一个任务(使用默认的任务组名，触发器名，触发器组名)
-	 * 
-	 * @param sched
-	 *            调度器
-	 * @param jobName
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void removeJob(Scheduler sched, String jobName) {
-		try {
-			TriggerKey triggerKey = new TriggerKey(jobName, TRIGGER_GROUP_NAME);
-			sched.pauseTrigger(triggerKey);// 停止触发器
-			sched.unscheduleJob(triggerKey);// 移除触发器
-			JobKey jobKey = new JobKey(jobName, JOB_GROUP_NAME);
-			sched.deleteJob(jobKey);// 删除任务
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler        调度器
+     * @param jobName
+     * @param jobGroupName
+     * @param triggerName
+     * @param triggerGroupName
+     * @Description: 移除一个任务
+     * @Title: QuartzManager.java
+     */
+    public static void removeJob(Scheduler scheduler, String jobName, String jobGroupName, String triggerName,
+                                 String triggerGroupName) {
+        try {
+            TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
+            scheduler.pauseTrigger(triggerKey);// 停止触发器
+            scheduler.unscheduleJob(triggerKey);// 移除触发器
+            JobKey jobKey = new JobKey(jobName, jobGroupName);
+            scheduler.deleteJob(jobKey);// 删除任务
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description: 移除一个任务
-	 * 
-	 * @param sched
-	 *            调度器
-	 * @param jobName
-	 * @param jobGroupName
-	 * @param triggerName
-	 * @param triggerGroupName
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void removeJob(Scheduler sched, String jobName, String jobGroupName, String triggerName,
-			String triggerGroupName) {
-		try {
-			TriggerKey triggerKey = new TriggerKey(triggerName, triggerGroupName);
-			sched.pauseTrigger(triggerKey);// 停止触发器
-			sched.unscheduleJob(triggerKey);// 移除触发器
-			JobKey jobKey = new JobKey(jobName, jobGroupName);
-			sched.deleteJob(jobKey);// 删除任务
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler 调度器
+     * @Description:启动所有定时任务
+     * @Title: QuartzManager.java
+     */
+    public static void startJobs(Scheduler scheduler) {
+        try {
+            scheduler.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @Description:启动所有定时任务
-	 * 
-	 * @param sched  调度器
-	 * 
-	 * @Title: QuartzManager.java
-	 */
-	public static void startJobs(Scheduler sched) {
-		try {
-			sched.start();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * @Description:关闭所有定时任务
-	 * 
-	 * @param sched
-	 *            调度器
-	 * 
-	 */
-	public static void shutdownJobs(Scheduler sched) {
-		try {
-			if (!sched.isShutdown()) {
-				sched.shutdown();
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * @param scheduler 调度器
+     * @Description:关闭所有定时任务
+     */
+    public static void shutdownJobs(Scheduler scheduler) {
+        try {
+            if (!scheduler.isShutdown()) {
+                scheduler.shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
